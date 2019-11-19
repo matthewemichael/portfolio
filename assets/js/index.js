f69c9fe4ca3792ff84d64e74e798e7f62bb9e2a8
Matt Michael <matthewemichael@gmail.com>
2019-11-19 14:07:32 -0600

    navbar with section target links working

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,17 @@ $('.nav').click(function() {
         scrollTop: ($('#' + id + '.section').offset().top)
     }, 1000);
 });
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+// $(window).on('resize', function() {
+//     if($(window).width() < 768) {
+//         $('nav').addClass('navbar-light bg-light');
+//     }
+//     else if($(window).width() > 768) {
+//         $('nav').removeClass('navbar-light bg-light')
+//     }
+// });
+
