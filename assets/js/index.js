01a744c7ad91f3a08851f3bd5570c7b8f0db9f0b
Matt Michael <matthewemichael@gmail.com>
2019-11-20 18:00:43 -0600

    added images, working on layout and styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,13 +8,3 @@ $('.nav').click(function() {
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
-
-// $(window).on('resize', function() {
-//     if($(window).width() < 768) {
-//         $('nav').addClass('navbar-light bg-light');
-//     }
-//     else if($(window).width() > 768) {
-//         $('nav').removeClass('navbar-light bg-light')
-//     }
-// });
-
