d6e2f6972bc6c5ec2ad30a8ba04b89d6734455bf
Matt Michael <matthewemichael@gmail.com>
2019-11-23 23:02:37 -0600

    navbar hidden until page scrolled

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,16 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function(){
+    $(".navbar").hide();
+    $(function () {
+        $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.navbar').fadeIn();
+        } else {
+            $('.navbar').fadeOut();
+        }
+        });
+    });
+});
